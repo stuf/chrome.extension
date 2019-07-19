78b49a7e1d656d7ddabb9fa03662cffcd5ab5733
Stefan Rimaila <stefan@rimaila.fi>
2019-07-19 13:53:02 +0200

    Clean up request logging in background script

--- src/background/index.js
+++ src/background/index.js
@@ -5,8 +5,10 @@ chrome.runtime.onConnect.addListener(port => {
 
   port.onMessage.addListener((msg, source, emit) => {
     console.groupCollapsed(msg.request.request.url);
-    console.log('data =>\t%O', msg.data);
     console.log('request =>\t%O', msg.request);
+    console.groupCollapsed('Request data');
+    console.log('%O', msg.data);
+    console.groupEnd();
     console.groupEnd();
   })
 });
